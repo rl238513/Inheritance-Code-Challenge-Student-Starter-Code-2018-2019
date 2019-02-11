252913b08939a5268e424927fde65658827a1922
EDUARDO MENDOZA <rl238513@rlas-macair-25.local>
2019-02-11 11:53:54 -0600

    Initial Commit

---
+++ InheritanceChallenge/AppleDevice.swift
@@ -0,0 +1,46 @@
+//
+//  AppleDevice.swift
+//  ClassificationAndInheritanceChallenge
+//
+//  Created by Robert Le Bruce on 6/9/18.
+//  Copyright (c) 2018 MobileMakers. All rights reserved.
+//
+
+import Foundation
+/***************************************************
+* Start Your Code Here For MVP
+***************************************************/
+class AppleDevice{
+    var name = String()
+    var model = String()
+    var batterLevel = Float()
+    var systemVertion = String()
+}
+    /***************************************************
+    * Start Your Code Here For Stretch #1
+    ***************************************************/
+
+    /***************************************************
+    * End Your Code Here For Stretch #1
+    ***************************************************/
+
+    /***************************************************
+    * Start Your Code Here For Stretch #2
+    ***************************************************/
+
+    /***************************************************
+    * End Your Code Here For Stretch #2
+    ***************************************************/
+
+    /***************************************************
+    * Start Your Code Here For Stretch #3
+    ***************************************************/
+
+    /***************************************************
+    * End Your Code Here For Stretch #3
+    ***************************************************/
+
+
+/***************************************************
+* End Your Code Here For MVP
+***************************************************/
---
+++ InheritanceChallenge/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  ClassificationAndInheritanceChallenge
+//
+//  Created by Robert Le Bruce on 6/6/18.
+//  Copyright (c) 2018 MobileMakers. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UITextView!
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        UIDevice.current.isBatteryMonitoringEnabled = true
+
+        let myMVPDevice = iPad()
+
+        myLabel.text = getDeviceInfo(myMVPDevice)
+    }
+
+    @IBAction func onStretch1Pressed(_ sender: UIButton)
+    {
+        let myFirstDevice = iPad()
+
+        myFirstDevice.name = UIDevice.current.name
+        myFirstDevice.model = UIDevice.current.model
+        myFirstDevice.batteryLevel = UIDevice.current.batteryLevel
+        myFirstDevice.systemVersion = UIDevice.current.systemVersion
+
+        myFirstDevice.orientationIsPortrait = UIDevice.current.orientation.isPortrait
+
+        myLabel.text = "Stretch #1 Completed\n\n" + getDeviceInfo(myFirstDevice)
+    }
+
+    @IBAction func onStretch2Pressed(_ sender: UIButton)
+    {
+        let name = UIDevice.current.name
+        let model = UIDevice.current.model
+        let batteryLevel = UIDevice.current.batteryLevel
+        let systemVersion = UIDevice.current.systemVersion
+        let screenWidth = (Float)(UIScreen.main.bounds.width)
+        let screenHeight = (Float)(UIScreen.main.bounds.height)
+        let orientationIsPortrait = UIDevice.current.orientation.isPortrait
+
+        //Uncomment for the two lines below for Stretch #2
+        //let mySecondDevice = iPad(Name: name, Model: model, BatteryLevel: batteryLevel, SystemVersion: systemVersion, ScreenWidth: screenWidth, ScreenHeight: screenHeight, Orientation: orientationIsPortrait)
+       // myLabel.text = "Stretch #2 Completed\n\n" + getDeviceInfo(mySecondDevice)
+    }
+
+    @IBAction func onStretch3Pressed(_ sender: UIButton)
+    {
+        let name = UIDevice.current.name
+        let model = UIDevice.current.model
+        let batteryLevel = UIDevice.current.batteryLevel
+        let systemVersion = UIDevice.current.systemVersion
+
+        //Uncomment for the six lines below for Stretch #3
+       // let myAppleDevice = AppleDevice(Name: name, Model: model, BatteryLevel: batteryLevel, SystemVersion: systemVersion)
+
+       // let myThirdDevice = iPad(AppleDevice: myAppleDevice)
+
+       // myThirdDevice.screenWidth = (Float)(UIScreen.main.bounds.width)
+       // myThirdDevice.screenHeight = (Float)(UIScreen.main.bounds.height)
+     //   myThirdDevice.orientationIsPortrait = UIDevice.current.orientation.isPortrait
+
+      //  myLabel.text = "Stretch #3 Completed\n\n" + getDeviceInfo(myThirdDevice)
+    }
+
+    func getDeviceInfo(_ device: iPad) -> String {
+        return  "Name: \(device.name)\n" +
+                "Model: \(device.model)\n" +
+                "Battery Level: \(device.batteryLevel)\n" +
+                "System Version: \(device.systemVersion)\n" +
+                "Screen Width: \(device.screenWidth)\n" +
+                "Screen Height: \(device.screenHeight)\n" +
+                "Orientation Is Portrait?: \(device.orientationIsPortrait)"
+    }
+}
+
